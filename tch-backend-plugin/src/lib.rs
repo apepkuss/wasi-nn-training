71b2f70af623841d5f1aadca39ebad4a17f6c301
Xin Liu <sam@secondstate.io>
2023-03-15 02:03:34 +0000

    chore: update tch-backend-plugin and test-backend-plugin
    Signed-off-by: Xin Liu <sam@secondstate.io>

---
+++ tch-backend-plugin/src/lib.rs
@@ -0,0 +1,207 @@
+use tch::Tensor;
+use wasmedge_sdk::{
+    error::HostFuncError,
+    host_function,
+    plugin::{ffi, PluginDescriptor, PluginVersion},
+    Caller, ImportObjectBuilder, ValType, WasmValue,
+};
+
+// A native function to be wrapped as a host function
+#[host_function]
+fn real_add(_: Caller, input: Vec<WasmValue>) -> Result<Vec<WasmValue>, HostFuncError> {
+    println!("Welcome! This is NaiveMath plugin.");
+
+    let t = Tensor::of_slice(&[3, 1, 4, 1, 5]);
+    let t = t * 2;
+    t.print();
+
+    if input.len() != 2 {
+        return Err(HostFuncError::User(1));
+    }
+
+    let a = if input[0].ty() == ValType::I32 {
+        input[0].to_i32()
+    } else {
+        return Err(HostFuncError::User(2));
+    };
+
+    let b = if input[1].ty() == ValType::I32 {
+        input[1].to_i32()
+    } else {
+        return Err(HostFuncError::User(3));
+    };
+
+    let c = a + b;
+    Ok(vec![WasmValue::from_i32(c)])
+}
+
+#[host_function]
+fn set_input_tensor(
+    caller: Caller,
+    input: Vec<WasmValue>,
+) -> Result<Vec<WasmValue>, HostFuncError> {
+    println!("This is `set_input_tensor` host function.");
+
+    let memory = caller.memory(0).expect("failed to get memory at idex 0");
+
+    if input.len() != 2 {
+        return Err(HostFuncError::User(1));
+    }
+
+    let a = if input[0].ty() == ValType::I32 {
+        input[0].to_i32()
+    } else {
+        return Err(HostFuncError::User(2));
+    };
+    println!("a: {a}");
+
+    let b = if input[1].ty() == ValType::I32 {
+        input[1].to_i32()
+    } else {
+        return Err(HostFuncError::User(2));
+    };
+    println!("b: {b}");
+
+    // println!(
+    //     "plugin: size of Array: {}",
+    //     std::mem::size_of::<protocol::Array>()
+    // );
+    // println!(
+    //     "plugin: size of *const u8: {}",
+    //     std::mem::size_of::<*const u8>()
+    // );
+    // println!("plugin: size of u8: {}", std::mem::size_of::<u8>());
+
+    // println!("plugin: offset: {}", a);
+    // let ptr = a as *const u8;
+    // println!("plugin: ptr: {:p}", ptr);
+
+    // println!("plugin: len: {}", b);
+
+    let data = memory
+        .data_pointer(a as u32, b as u32)
+        .expect("failed to get data from linear memory");
+
+    // let slice = unsafe { std::slice::from_raw_parts(data, b as usize) };
+    // println!("first number: {:?}", slice);
+
+    let slice = unsafe { std::slice::from_raw_parts(data, 16) };
+    println!("slice1: {:?}", slice);
+
+    // extract the first (offset, size) from the linear memory
+    let offset1 = i32::from_le_bytes(slice[0..4].try_into().unwrap());
+    let size1 = i32::from_le_bytes(slice[4..8].try_into().unwrap());
+    println!("offset1: {}, size1: {}", offset1, size1);
+    // extract the first sequence of numbers from the linear memory by (offset, size)
+    let num1 = memory
+        .read(offset1 as u32, size1 as u32)
+        .expect("failed to read numbers");
+    println!("num1: {:?}", num1);
+
+    Ok(vec![])
+}
+
+/// Defines Plugin module instance
+unsafe extern "C" fn create_test_module(
+    _arg1: *const ffi::WasmEdge_ModuleDescriptor,
+) -> *mut ffi::WasmEdge_ModuleInstanceContext {
+    let module_name = "naive-math";
+    let import = ImportObjectBuilder::new()
+        // add a function
+        .with_func::<(i32, i32), i32>("add", real_add)
+        .expect("failed to create host function: add")
+        .with_func::<(i32, i32), ()>("set_input_tensor", set_input_tensor)
+        .expect("failed to create host function: set_input_tensor")
+        .build(module_name)
+        .expect("failed to create import object");
+
+    let boxed_import = Box::new(import);
+    let import = Box::leak(boxed_import);
+
+    import.as_raw_ptr() as *mut _
+}
+
+/// Defines PluginDescriptor
+#[export_name = "WasmEdge_Plugin_GetDescriptor"]
+pub extern "C" fn plugin_hook() -> *const ffi::WasmEdge_PluginDescriptor {
+    let name = "naive_math_plugin";
+    let desc = "this is naive math plugin";
+    let version = PluginVersion::new(0, 0, 0, 0);
+    let plugin_descriptor = PluginDescriptor::new(name, desc, version)
+        .expect("Failed to create plugin descriptor")
+        .add_module_descriptor(
+            "naive_math_module",
+            "this is naive math module",
+            Some(create_test_module),
+        )
+        .expect("Failed to add module descriptor");
+
+    let boxed_plugin = Box::new(plugin_descriptor);
+    let plugin = Box::leak(boxed_plugin);
+
+    plugin.as_raw_ptr()
+}
+
+// * interface protocol
+
+mod protocol {
+
+    #[repr(C)]
+    #[derive(Copy, Clone, Debug)]
+    pub struct Array {
+        pub data: *const u8,
+        pub size: i32,
+    }
+
+    #[repr(C)]
+    #[derive(Copy, Clone, Debug)]
+    pub struct Tensor<'a> {
+        pub dimensions: TensorDimensions<'a>,
+        pub type_: TensorType,
+        pub data: TensorData<'a>,
+    }
+
+    pub type TensorData<'a> = &'a [u8];
+    pub type TensorDimensions<'a> = &'a [u32];
+
+    #[repr(transparent)]
+    #[derive(Copy, Clone, Hash, Eq, PartialEq, Ord, PartialOrd)]
+    pub struct TensorType(u8);
+    pub const TENSOR_TYPE_F16: TensorType = TensorType(0);
+    pub const TENSOR_TYPE_F32: TensorType = TensorType(1);
+    pub const TENSOR_TYPE_U8: TensorType = TensorType(2);
+    pub const TENSOR_TYPE_I32: TensorType = TensorType(3);
+    impl TensorType {
+        pub const fn raw(&self) -> u8 {
+            self.0
+        }
+
+        pub fn name(&self) -> &'static str {
+            match self.0 {
+                0 => "F16",
+                1 => "F32",
+                2 => "U8",
+                3 => "I32",
+                _ => unsafe { core::hint::unreachable_unchecked() },
+            }
+        }
+        pub fn message(&self) -> &'static str {
+            match self.0 {
+                0 => "",
+                1 => "",
+                2 => "",
+                3 => "",
+                _ => unsafe { core::hint::unreachable_unchecked() },
+            }
+        }
+    }
+    impl std::fmt::Debug for TensorType {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.debug_struct("TensorType")
+                .field("code", &self.0)
+                .field("name", &self.name())
+                .field("message", &self.message())
+                .finish()
+        }
+    }
+}
---
+++ test-tch-backend/src/main.rs
@@ -0,0 +1,165 @@
+use byteorder::{LittleEndian, ReadBytesExt};
+use std::io::Cursor;
+
+mod plugin {
+    #[link(wasm_import_module = "naive-math")]
+    extern "C" {
+        pub fn add(x: i32, y: i32) -> i32;
+        pub fn set_input_tensor(x: i32, y: i32);
+    }
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let x: i32 = args[1].parse().unwrap();
+    let y: i32 = args[2].parse().unwrap();
+
+    let res = unsafe { plugin::add(x, y) };
+    println!("{x} + {y} = {res}");
+
+    let nd_array: ndarray::Array<f32, _> =
+        ndarray::array![[1., 2., 3.], [2., 3., 1.], [3., 1., 2.]];
+    println!("original nd_array:");
+    println!("{:?}\n", &nd_array);
+
+    println!("converting ndarry -> bytes");
+    let floats = nd_array
+        .as_slice()
+        .expect("failed to convert ndarray to slice");
+    let bytes = to_byte_slice(floats);
+    println!("size of bytes: {}", bytes.len());
+
+    // println!("size of Array: {}", std::mem::size_of::<protocol::Array>());
+    // println!("size of *const u8: {}", std::mem::size_of::<*const u8>());
+    // println!("size of u8: {}", std::mem::size_of::<u8>());
+
+    // let nums = [1u8, 2, 3];
+    // let ptr = &nums as *const u8;
+    // println!("ptr address: {:p}", ptr);
+    // let offset = ptr as i32;
+    // println!("offset: {}", offset);
+    // unsafe { plugin::set_input_tensor(offset, 3) };
+
+    // =======
+
+    println!("size of Array: {}", std::mem::size_of::<protocol::Array>());
+
+    let data1 = [1u8, 2, 3, 4, 5];
+    let arr1 = protocol::Array {
+        data: &data1 as *const _,
+        size: data1.len() as i32,
+    };
+    let data2 = [6u8, 7, 8];
+    let arr2 = protocol::Array {
+        data: &data2 as *const _,
+        size: data2.len() as i32,
+    };
+
+    let arrs = [arr1, arr2];
+    let ptr = &arrs as *const _ as usize as i32;
+
+    unsafe {
+        plugin::set_input_tensor(
+            ptr,
+            (arrs.len() * std::mem::size_of::<protocol::Array>()) as i32,
+        );
+    }
+}
+
+fn to_byte_slice<'a>(floats: &'a [f32]) -> &'a [u8] {
+    unsafe { std::slice::from_raw_parts(floats.as_ptr() as *const _, floats.len() * 4) }
+}
+
+pub fn bytes_to_f32_vec(data: Vec<u8>) -> Vec<f32> {
+    let chunks: Vec<&[u8]> = data.chunks(4).collect();
+    let v: Vec<f32> = chunks
+        .into_iter()
+        .map(|c| {
+            let mut rdr = Cursor::new(c);
+            rdr.read_f32::<LittleEndian>().expect("failed to read")
+        })
+        .collect();
+
+    v.into_iter().collect()
+}
+
+pub fn f32_vec_to_bytes(data: Vec<f32>) -> Vec<u8> {
+    let sum: f32 = data.iter().sum();
+    // log::info!(
+    //     "f32_vec_to_bytes: flatten output tensor contains {} elements with sum {}",
+    //     data.len(),
+    //     sum
+    // );
+    let chunks: Vec<[u8; 4]> = data.into_iter().map(|f| f.to_le_bytes()).collect();
+    let result: Vec<u8> = chunks.iter().flatten().copied().collect();
+
+    // log::info!(
+    //     "f32_vec_to_bytes: flatten byte output tensor contains {} elements",
+    //     result.len()
+    // );
+    result
+}
+
+// * interface protocol
+
+mod protocol {
+
+    #[repr(C)]
+    #[derive(Copy, Clone, Debug)]
+    pub struct Array {
+        pub data: *const u8, // 4 bytes
+        pub size: i32,       // 4 bytes
+    }
+
+    #[repr(C)]
+    #[derive(Copy, Clone, Debug)]
+    pub struct Tensor<'a> {
+        pub dimensions: TensorDimensions<'a>,
+        pub ty: TensorType,
+        pub data: TensorData<'a>,
+    }
+
+    pub type TensorData<'a> = &'a [u8];
+    pub type TensorDimensions<'a> = &'a [u32];
+
+    #[repr(transparent)]
+    #[derive(Copy, Clone, Hash, Eq, PartialEq, Ord, PartialOrd)]
+    pub struct TensorType(u8);
+    pub const TENSOR_TYPE_F16: TensorType = TensorType(0);
+    pub const TENSOR_TYPE_F32: TensorType = TensorType(1);
+    pub const TENSOR_TYPE_U8: TensorType = TensorType(2);
+    pub const TENSOR_TYPE_I32: TensorType = TensorType(3);
+    impl TensorType {
+        pub const fn raw(&self) -> u8 {
+            self.0
+        }
+
+        pub fn name(&self) -> &'static str {
+            match self.0 {
+                0 => "F16",
+                1 => "F32",
+                2 => "U8",
+                3 => "I32",
+                _ => unsafe { core::hint::unreachable_unchecked() },
+            }
+        }
+        pub fn message(&self) -> &'static str {
+            match self.0 {
+                0 => "",
+                1 => "",
+                2 => "",
+                3 => "",
+                _ => unsafe { core::hint::unreachable_unchecked() },
+            }
+        }
+    }
+    impl std::fmt::Debug for TensorType {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.debug_struct("TensorType")
+                .field("code", &self.0)
+                .field("name", &self.name())
+                .field("message", &self.message())
+                .finish()
+        }
+    }
+}
